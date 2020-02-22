7199982bb8ea12b2c28403d1503c3556d8682915
tehlordvortex <tehlordvortex@outlook.com>
2020-02-22 18:53:18 +0100

    add storybook and other libs

---
+++ src/stories/0-Welcome.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default {
+  title: "Welcome"
+};
+
+export const ToStorybook = () => <p>Samedaycustom Storybook</p>;
+
+ToStorybook.story = {
+  name: "to Storybook"
+};
