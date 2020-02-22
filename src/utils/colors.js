a60ac957f350b38e213e679da8758fea73796d4d
tehlordvortex <tehlordvortex@outlook.com>
2020-02-22 18:59:14 +0100

    add extracted colors from mockup

---
+++ src/utils/colors.js
@@ -0,0 +1,30 @@
+export const ColorPalette = {
+  sidebarBackground: `#196BD8`,
+  sidebarText: `#2268D8`,
+  tabIndicator: `#007AFE`,
+  tabText: `#007AFE`,
+  badgeBackground: {
+    default: `#007BFF`,
+    danger: `#D80B0B`,
+    light: `#529DED`
+  },
+  pageBackground: `#FCFCFC`,
+  dropdownTriggerBackground: {
+    default: `#007BFF`,
+    transparent: `transparent`
+  },
+  dropdownTriggerColor: {
+    default: `#fff`,
+    transparent: `#1A3A54`
+  },
+  red: `#FF0000`,
+  white: `#FFFFFF`,
+  blue: `#196BD8`,
+  green: `#009B2A`,
+  gray: {
+    veryLight: `#C1C1C1`,
+    background: `#EEEEEE`,
+    disabled: `#D0D0D0`,
+    text: `#707070`
+  }
+};
