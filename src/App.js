9aa6ba5616ba0672cd417bf7a8219e307176b62e
tehlordvortex <tehlordvortex@outlook.com>
2020-02-23 19:50:55 +0100

    update App component to render the shell

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Sidebar } from "./components/Sidebar";
+import styled from "styled-components";
+import { Header } from "./components/Header";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { ColorPalette } from "./utils/colors";
+
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+  overflow-x: hidden;
+`;
+
+const ContentArea = styled.div`
+  width: calc(100vw - 7.15rem);
+  min-height: 100vh;
+  margin-left: ${props => props.sidebarWidth};
+  background-color: ${ColorPalette.pageBackground};
+  transition: 0.3s ease-out;
+  display: flex;
+  flex-direction: column;
+`;
+
+const PageContent = styled.main`
+  padding: 2rem 7.65rem 2rem 13rem;
+  height: 100%;
+`;
 
 function App() {
+  const [sidebarWidth, setSidebarWidth] = useState(`7.15rem`);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Wrapper>
+        <Sidebar onWidthChange={setSidebarWidth} />
+        <ContentArea sidebarWidth={sidebarWidth}>
+          <Header />
+          <PageContent>
+            <Switch></Switch>
+          </PageContent>
+        </ContentArea>
+      </Wrapper>
+    </Router>
   );
 }
 
