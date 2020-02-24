61addc25f8dc720ee40ce99b904ac57e5682a1c5
tehlordvortex <tehlordvortex@outlook.com>
2020-02-24 15:14:35 +0100

    add back button to TopPanel

--- src/components/TopPanel.js
+++ src/components/TopPanel.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import { ColorPalette } from "../utils/colors";
+import { useHistory } from "react-router-dom";
+import { ReactComponent as BackIcon } from "../icons/right-arrow.svg";
 
 const TopPanelContainer = styled.div`
   display: flex;
@@ -28,12 +30,76 @@ const TopPanelTitle = styled.h1`
   color: ${ColorPalette.black};
 `;
 
-export const TopPanel = ({ icon, title, children, ...props }) => (
-  <TopPanelContainer {...props}>
-    <TopPanelSection>
-      <TopPanelIcon>{icon}</TopPanelIcon>
-      <TopPanelTitle>{title}</TopPanelTitle>
-    </TopPanelSection>
-    <TopPanelSection>{children}</TopPanelSection>
-  </TopPanelContainer>
-);
+const BackButton = styled.button`
+  cursor: pointer;
+  border: none;
+  background-color: transparent;
+  display: flex;
+  align-items: center;
+`;
+
+const BackIconWrapper = styled.span`
+  background-color: ${ColorPalette.topPanelIcon};
+  color: ${ColorPalette.white};
+  border-radius: 50%;
+  width: 3.2rem;
+  height: 3.2rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 1rem;
+
+  svg {
+    width: 30px;
+  }
+`;
+
+const BackButtonText = styled.span`
+  text-align: left;
+  font-weight: 500;
+  font-size: 1.5rem;
+  line-height: 1.23;
+  /* font-family: Nunito, sans-serif; */
+  letter-spacing: 0.015rem;
+  color: ${ColorPalette.gray.text};
+`;
+
+const TopPanelDivider = styled.div`
+  height: 2rem;
+  width: 1px;
+  background-color: ${ColorPalette.black};
+  margin: 0 1rem;
+`;
+
+export const TopPanel = ({
+  icon,
+  title,
+  children,
+  showBackButton = false,
+  ...props
+}) => {
+  const { goBack } = useHistory();
+  const handleBack = () => {
+    goBack();
+  };
+  return (
+    <TopPanelContainer {...props}>
+      <TopPanelSection>
+        {showBackButton && (
+          <React.Fragment>
+            <BackButton onClick={handleBack}>
+              <BackIconWrapper>
+                <BackIcon />
+              </BackIconWrapper>
+              <BackButtonText>Back</BackButtonText>
+            </BackButton>
+            <TopPanelDivider />
+          </React.Fragment>
+        )}
+        <TopPanelIcon>{icon}</TopPanelIcon>
+        <TopPanelTitle>{title}</TopPanelTitle>
+      </TopPanelSection>
+      <TopPanelSection>{children}</TopPanelSection>
+    </TopPanelContainer>
+  );
+};
--- src/components/TopPanel.stories.js
+++ src/components/TopPanel.stories.js
@@ -2,20 +2,25 @@ import React from "react";
 import { TopPanel } from "./TopPanel";
 import { ColorPalette } from "../utils/colors";
 import { ReactComponent as ProductIcon } from "../icons/product.svg";
+import { MemoryRouter } from "react-router";
+import { Search } from "./Search";
+import { withState } from "@dump247/storybook-state";
 
 export default {
   title: "TopPanel",
   component: TopPanel,
   decorators: [
     storyFn => (
-      <div
-        style={{
-          padding: "2rem",
-          backgroundColor: ColorPalette.pageBackground
-        }}
-      >
-        {storyFn()}
-      </div>
+      <MemoryRouter>
+        <div
+          style={{
+            padding: "2rem",
+            backgroundColor: ColorPalette.pageBackground
+          }}
+        >
+          {storyFn()}
+        </div>
+      </MemoryRouter>
     )
   ]
 };
@@ -23,3 +28,20 @@ export default {
 export const TopPanelStory = () => (
   <TopPanel icon={<ProductIcon />} title="Production House"></TopPanel>
 );
+
+export const TopPanelWithBackButton = () => (
+  <TopPanel
+    icon={<ProductIcon />}
+    title="Production House"
+    showBackButton
+  ></TopPanel>
+);
+
+export const TopPanelWithActions = withState({ value: "" })(({ store }) => (
+  <TopPanel icon={<ProductIcon />} title="Production House">
+    <Search
+      value={store.state.value}
+      onChange={value => store.set({ value })}
+    />
+  </TopPanel>
+));
